10a7c23b2325787714e256b822f3fe5041719106
Kiryl Stasevich <kstasevich@tenna.com>
2023-11-28 11:30:02 +0100

    fix: refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import './App.scss'
-import {Route, Routes} from "react-router-dom";
-import {TablePage} from "./pages/TablePage.tsx";
-import {AddEntityPage} from "./pages/AddEntityPage.tsx";
-import {EditEntityPage} from "./pages/EditEntityPage.tsx";
-import {Layout} from "./components/Layout.tsx";
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import { TablePage } from './pages/TablePage.tsx';
+import { AddEntityPage } from './pages/AddEntityPage.tsx';
+import { EditEntityPage } from './pages/EditEntityPage.tsx';
+import { Layout } from './components/layouts/MainLayout/Layout.tsx';
 
 function App() {
   // const [count, setCount] = useState(0)
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route path="/" element={<TablePage />}></Route>
-                    <Route path="/add" element={<AddEntityPage />}></Route>
-                    <Route path="/edit" element={<EditEntityPage />}></Route>
-                </Route>
-            </Routes>
-        </>
-    )
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<TablePage />}></Route>
+          <Route path="/add" element={<AddEntityPage />}></Route>
+          <Route path="/edit" element={<EditEntityPage />}></Route>
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/layouts/MainLayout/Layout.tsx
@@ -0,0 +1,11 @@
+// import NavBar from "./NavBar.tsx";
+// import {Outlet} from "react-router-dom";
+import { Sidebar } from '../../Sidebar.tsx';
+
+export const Layout = () => {
+  return (
+    // <>
+    <Sidebar></Sidebar>
+    // </>
+  );
+};
